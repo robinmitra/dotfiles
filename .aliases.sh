0cbd61a6b1ebd98549e0cdefbb7a6ef3721c8dd6
Robin Mitra <robinmitra1@gmail.com>
2018-04-22 20:21:54 +0100

    Add shell and git aliases, and refine git log output. Also make commit signing required by default.

--- .aliases.sh
+++ .aliases.sh
@@ -33,6 +33,7 @@ alias fsize="stat -f \"%z bytes\""
 
 alias zr="source ~/.zshrc"
 alias zc="vim ~/.zshrc"
+alias zp="vim ~/.zpreztorc"
 alias za="vim ~/.aliases.sh"
 
 #############
@@ -47,8 +48,8 @@ alias play="cd $PLAY"
 #######
 
 alias gpo="gp origin"
-alias glog="git log --color --graph --pretty=format:'%Cred%h%Creset %C(dim cyan)▹%Creset %Cgreen%cd%Creset %C(dim cyan)▹%Creset %C(bold blue)%an %C(black)<%ae>%Creset %C(auto)%+D%Creset %n%s%n' --date=format:'%a %d %b %y, %H:%m'"
-alias glogc="git log --color --graph --pretty=format:'%Cred%h%Creset%C(yellow)%d%Creset %C(dim cyan)▹%Creset %s %C(dim cyan)▹%Creset %Cgreen%cr%Creset %C(dim cyan)▹%Creset %C(bold blue) %an%Creset'"
+alias glog="git log --color --graph --pretty=format:'%Cred%h%Creset %C(dim white)▹%Creset %C(green)%cr%Creset %C(dim green)%cd%Creset %C(dim white)▹%Creset %C(magenta)%an %C(dim magenta)<%ae>%Creset %C(dim white)▹%Creset %C(cyan)%G?%Creset %C(dim cyan)%GS%GK%Creset %C(auto)%+D%Creset %n%s%n' --date=format:'%a %d %b %y, %H:%m'"
+alias glogc="git log --color --graph --pretty=format:'%Cred%h%Creset%C(yellow)%d%Creset %C(dim white)▹%Creset %s %C(dim white)▹%Creset %Cgreen%cr%Creset %C(dim white)▹%Creset %C(magenta)%an%Creset %C(dim white)▹%Creset %C(cyan)%G?%Creset'"
 alias gla="git config -l | grep alias | cut -c 7-"
 
 ##############
@@ -67,3 +68,4 @@ alias -s rb=vim
 alias -s xml=vim
 alias -s php=vim
 alias -s json=vim
+alias -s go='go run'
